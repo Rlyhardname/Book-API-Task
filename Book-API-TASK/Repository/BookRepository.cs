472ae05ea44eb3ba99f29b37fa455fcafb350657
Ivan Dimitrov <mmo_unstable@abv.bg>
2025-02-23 13:03:44 +0200

    -create config variables for db url

--- Book-API-TASK/Repository/BookRepository.cs
+++ Book-API-TASK/Repository/BookRepository.cs
@@ -5,11 +5,22 @@ namespace Book_API_TASK.Repository;
 
 public class BookRepository : DbContext
 {
+
+    /// <summary>
+    ///  Switch with your server, usually localhost
+    /// </summary>
+    public static readonly string SERVER = "localhost\\SQLEXPRESS01";
+    
+    /// <summary>
+    /// switch with your database name
+    /// </summary>
+    public static readonly string DATABASE_NAME = "BookDB";
+    
     public DbSet<EBook> Ebooks { get; set; }
     
     protected override void OnConfiguring(DbContextOptionsBuilder options)
     {
-        options.UseSqlServer("Server=localhost\\SQLEXPRESS01;Database=BookDB;Trusted_Connection=True;TrustServerCertificate=True;");
+        options.UseSqlServer($"Server={SERVER};Database={DATABASE_NAME};Trusted_Connection=True;TrustServerCertificate=True;");
     }
     
     protected override void OnModelCreating(ModelBuilder modelBuilder)
